c57c928f46b71d2cac8b75562fe11f441d3f1c61
pej0918 <pej0918@ewhain.net>
2024-08-27 15:46:15 +0900

    ADD VAD

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -188,6 +188,62 @@ class MVTecDataset_no_seg(torch.utils.data.Dataset):
         #print(img.shape,img_type)
         return img, label, img_type
     
+
+#######################  VAD   ###############################
+
+# Custom dataset class for VAD dataset
+class VADDataset_no_seg(torch.utils.data.Dataset):
+    def __init__(self, root, transform, phase):
+        if phase == 'train':
+            self.img_path = os.path.join(root, 'train')
+        else:
+            self.img_path = os.path.join(root, 'test')
+        self.transform = transform
+        # load dataset
+        self.img_paths, self.labels, self.types = self.load_dataset()  # self.labels => good : 0, anomaly : 1
+
+    def load_dataset(self):
+
+        img_tot_paths = []
+        tot_labels = []
+        tot_types = []
+
+        defect_types = os.listdir(self.img_path)
+
+        for defect_type in defect_types:
+            if defect_type == 'good':
+                img_paths = glob.glob(os.path.join(self.img_path, defect_type) + "/*.png")
+                img_tot_paths.extend(img_paths)
+                tot_labels.extend([0] * len(img_paths))
+                tot_types.extend(['good'] * len(img_paths))
+            else:
+                img_paths = glob.glob(os.path.join(self.img_path, defect_type) + "/*.png")
+                img_paths.sort()
+                img_tot_paths.extend(img_paths)
+                tot_labels.extend([1] * len(img_paths))
+                tot_types.extend([defect_type] * len(img_paths))
+
+        #assert len(img_tot_paths) == len(gt_tot_paths), "Something wrong with test and ground truth pair!"
+
+        return img_tot_paths, tot_labels, tot_types
+
+    def __len__(self):
+        return len(self.img_paths)
+
+    def __getitem__(self, idx):
+        img_path,  label, img_type = self.img_paths[idx],  self.labels[idx], self.types[idx]
+        img = Image.open(img_path).convert('RGB')
+
+        img = np.array(img)
+        #print(img.shape)
+        img = Image.fromarray(img)
+
+        img = self.transform(img)
+
+        #assert img.size()[1:] == gt.size()[1:], "image.size != gt.size !!!"
+        #print(img.shape,img_type)
+        return img, label, img_type
+    
 #######################  visA  ###############################
     
 # Custom dataset class for visA dataset
